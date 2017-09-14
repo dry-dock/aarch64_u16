d4f35e6be711c99cf9391341bd5c3ad8f0d41f0d
Niranjan Godbole <niranjan8192@gmail.com>
2017-09-14 09:40:30 +0000

    remove unused oracle8 install scripts

--- java/install.sh
+++ java/install.sh
@@ -15,13 +15,3 @@ apt-get update -qq
 apt-get install -qq icedtea-8-plugin=1.6.2-3ubuntu1
 update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-arm64/jre/bin/javaws
 
-# echo "================ Installing oracle-java8-installer ================="
-# echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-# add-apt-repository -y ppa:webupd8team/java
-# apt-get update -qq
-# apt-get install -qq -y oracle-java8-installer=8u144-1~webupd8~0
-# update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
-# update-alternatives --set javac /usr/lib/jvm/java-8-oracle/bin/javac
-# update-alternatives --set javaws /usr/lib/jvm/java-8-oracle/jre/bin/javaws
-# echo 'export JAVA_HOME=/usr/lib/jvm/java-8-oracle' >> $HOME/.bashrc
-# echo 'export PATH=$PATH:/usr/lib/jvm/java-8-oracle/jre/bin' >> $HOME/.bashrc
