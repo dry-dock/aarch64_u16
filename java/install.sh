418d78cae1ab688c5e2797cb5957d8f5dc24af1c
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-11-30 20:29:06 +0530

    updates jdk version

--- java/install.sh
+++ java/install.sh
@@ -7,11 +7,10 @@ apt-get install -qq default-jdk=2:1.8-56ubuntu2
 echo "================= Installing openjdk-8-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update -qq
-apt-get install -qq -y openjdk-8-jdk=8u131-b11-2ubuntu1.16.04.3
+apt-get install -qq -y openjdk-8-jdk=8u151-b12-0ubuntu0.16.04.2
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-arm64/jre/bin/java
 update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-arm64/bin/javac
 add-apt-repository ppa:maarten-fonville/ppa
 apt-get update -qq
 apt-get install -qq icedtea-8-plugin=1.6.2-3ubuntu1
 update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-arm64/jre/bin/javaws
-
