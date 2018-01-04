334fc75f37040bf8ddb4df990177d74542363fba
Bharath Upadhya <1992bharath@gmail.com>
2018-01-04 14:54:49 +0530

    adds -y flag to add-apt-repository

--- java/install.sh
+++ java/install.sh
@@ -10,7 +10,7 @@ apt-get update -qq
 apt-get install -qq -y openjdk-8-jdk=8u151-b12-0ubuntu0.16.04.2
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-arm64/jre/bin/java
 update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-arm64/bin/javac
-add-apt-repository ppa:maarten-fonville/ppa
+add-apt-repository -y ppa:maarten-fonville/ppa
 apt-get update -qq
 apt-get install -qq icedtea-8-plugin=1.6.2-3ubuntu1
 update-alternatives --set javaws /usr/lib/jvm/java-8-openjdk-arm64/jre/bin/javaws
