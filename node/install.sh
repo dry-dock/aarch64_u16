d792635328e93fb144171677101f94d583d6a042
Devashish <devashish.86@gmail.com>
2018-04-25 13:28:53 +0530

    fixing nodejs

--- node/install.sh
+++ node/install.sh
@@ -11,10 +11,10 @@ echo "source ~/.nvm/nvm.sh" >> /etc/drydock/.env
 
 echo "================= Installing nodejs 8.11.1 ================="
 curl -sSL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -qq -y nodejs=8.11.1
+sudo apt-get install -qq -y nodejs
 
 echo "================= Installing latest yarn ==================="
 sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
 echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update -qq
-sudo apt-get install -qq -qq -y yarn=1.6.0
+sudo apt-get install -qq -y yarn=1.6.0
