2da3ec4178dddd0bde8fc3946e0c2113c67417b0
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-28 12:22:53 +0530

    Update install.sh

--- node/install.sh
+++ node/install.sh
@@ -9,7 +9,7 @@ export NVM_DIR="/root/.nvm"
 # add source of nvm to .bashrc - allows user to use nvm as a command
 echo "source ~/.nvm/nvm.sh" >> /etc/drydock/.env
 
-echo "================= Installing nodejs 8.11.1 ================="
+echo "================= Installing nodejs 8.11 ================="
 curl -sSL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -qq -y nodejs
 
@@ -17,4 +17,4 @@ echo "================= Installing latest yarn ==================="
 sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
 echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update -qq
-sudo apt-get install -qq -y yarn=1.5*
+sudo apt-get install -qq -y yarn=1.6*
