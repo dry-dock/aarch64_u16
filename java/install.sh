7b78b8fc1b7d5efa1e51532db3eb55fb447dee4f
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-23 15:19:57 +0530

    Update install.sh

--- java/install.sh
+++ java/install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 
-echo "================= Installing default-jdk & jre ==================="
-apt-get install -qq default-jre=2:1.8*
-apt-get install -qq default-jdk=2:1.8*
-
 echo "================= Installing openjdk-10-jdk ==================="
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update -qq
